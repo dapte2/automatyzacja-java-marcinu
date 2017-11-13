9d5b23efa2e1bcbe6695b4e4f85cb6411822249c
Marcin Uhlenberg <4ulek@wp.pl>
2017-11-13 14:44:10 +0100

    zmiana struktóry

---
+++ src/main/java/Hello.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Hello{
+    public static void main(String[] args){
+
+        Scanner skaner = new Scanner(System.in); //obiekt do odebrania danych od użytkownika
+        String opcja;
+
+        System.out.println("Witaj świecie!");
+        do {
+
+            System.out.println( "Jeżeli chcesz zakończyć wpisz exit i naciśnij enter ");
+            opcja = skaner.next();
+        } while (!opcja.equalsIgnoreCase("exit"));
+
+        System.out.println("Koniec, dziękuję za współpracę.");
+
+
+    }
+
+
+
+
+}
