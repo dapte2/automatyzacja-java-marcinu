e7117f1284d2cf88155e87e080d89c38cf3b5abc
Marcin Uhlenberg <4ulek@wp.pl>
2017-11-13 14:17:56 +0100

    zadanie2

---
+++ src/main/java/codesprinters/autojava/Shape.java
@@ -0,0 +1,13 @@
+package codesprinters.autojava;
+
+/**
+ * Created by Michal on 2016-04-01.
+ */
+public interface Shape {
+
+    //private
+    public double getArea();
+
+    public String getName();
+
+}
