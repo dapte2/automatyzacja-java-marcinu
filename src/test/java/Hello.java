9e25b4f8118e335cc79e41281f38a9616d196e64
Marcin Uhlenberg <4ulek@wp.pl>
2017-11-13 10:58:21 +0100

    zadanie1

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Hello{
     public static void main(String[] args){
-        System.out.print("Hello World");
+
+        Scanner skaner = new Scanner(System.in); //obiekt do odebrania danych od użytkownika
+        String opcja;
+
+        System.out.println("Witaj świecie!");
+        do {
+
+            System.out.println( "Jeżeli chcesz zakończyć wpisz exit i naciśnij enter " + "jeśli nie, wybierz inny znak. ");
+            opcja = skaner.next();
+        } while (!opcja.equalsIgnoreCase("exit"));
+
+        System.out.println("Koniec, dziękuję za współpracę.");
+
+
     }
+
+
+
+
 }
